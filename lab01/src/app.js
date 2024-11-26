ac43f4a6dbb3eb1c444575abed5f92575c0e5288
Polcia <witkowska.pola@gmail.com>
2024-11-26 11:43:49 +0100

    zaczete

---
+++ lab01/src/app.js
@@ -0,0 +1,42 @@
+async function fetchPokemonList() {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
+        if (!response.ok) {
+            throw new Error(`Nie udało się pobrać danych o pokemonach, status: ${response.status}`);
+        }
+        const list = await response.json();
+        return list['results'].map(pokemon => pokemon.name);
+    } catch (error) {
+        console.error('Wystąpił błąd: ', error);
+    }  
+}
+
+async function fetchPokemon(name) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
+        if (!response.ok) {
+            throw new Error(`Nie udało się pobrać danych o pokemonie, status: ${response.status}`);
+        }
+        const pokemon = await response.json();
+        const result = {
+            name: pokemon.name,
+            types: pokemon.types.map(type => type.type.name),
+            abilities: pokemon.abilities.map(ability => ability.ability.name),
+            base_experience: pokemon.base_experience,
+            height: pokemon.height,
+            weight: pokemon.weight,
+        }
+        return result;
+    } catch (error) {
+        console.error('Wystąpił błąd: ', error);
+    }  
+}
+
+function general() {
+    document.getElementById("search").addEventListener("click", fetchPokemon)
+}
+
+
+// fetchPokemonList().then(list => console.log(list));
+// fetchPokemon('charizard').then(pokemon => console.log(pokemon));
+general();
