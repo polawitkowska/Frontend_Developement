91b2188d615888967945d565ac654ac21e514b0e
Pola Witkowska <witkowska.pola@gmail.com>
2024-12-09 22:52:23 +0100

    async functions

--- lab02/src/app.js
+++ lab02/src/app.js
@@ -1,113 +1,41 @@
+let pokemonList = [];
+let selectedPokemon = null;
+
 async function fetchPokemonList() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
+  const data = await response.json();
+  pokemonList = data.results;
+  renderApp();
+}
+
+async function fetchPokemonDetails(pokemonName) {
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
     if (!response.ok) {
-      throw new Error(
-        `Nie udało się pobrać danych o pokemonach, status: ${response.status}`
-      );
+      throw new Error("Nie znaleziono Pokemona");
     }
-    const list = await response.json();
-    return list["results"].map((pokemon) => pokemon.name);
+    const data = await response.json();
+    selectedPokemon = {
+      name: data.name,
+      id: data.id,
+      image: data.sprites.front_default,
+      types: data.types.map((type) => type.type.name),
+      base_stats: data.stats.map(
+        (stat) => `${stat.stat.name}: ${stat.base_stat}`
+      ),
+      height: data.height,
+      weight: data.weight,
+    };
+    document.getElementById("error").innerHTML = ``;
+    renderApp();
   } catch (error) {
-    console.error("Wystąpił błąd: ", error);
+    document.getElementById("error").innerHTML = `<p>${error.message}</p>`;
+    selectedPokemon = null;
+    renderApp();
   }
 }
-
-async function fetchPokemon(name) {
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}/`);
-    if (!response.ok) {
-      throw new Error(
-        `Nie udało się pobrać danych o pokemonie, status: ${response.status}`
-      );
-    } else {
-      const pokemon = await response.json();
-      const result = {
-        name: pokemon.name,
-        id: pokemon.id,
-        form: pokemon.forms.map((form) => form.url),
-        types: pokemon.types.map((type) => type.type.name),
-        // abilities: pokemon.abilities.map((ability) => ability.ability.name),
-        base_stats: pokemon.stats.map((stat) => stat.base_stat),
-        height: pokemon.height,
-        weight: pokemon.weight,
-        image: pokemon.sprites.front_default,
-      };
-
-      return result;
-    }
-  } catch (error) {
-    console.error("Wystąpił błąd: ", error);
-  }
-}
-
-async function displayPokemonList() {
-  const pokemonListElement = document.getElementById("pokemon-list-element");
-  const pokemonList = await fetchPokemonList();
-
-  pokemonListElement.innerHTML = ``;
-
-  pokemonList.forEach((pokemon) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = pokemon;
-    listItem.addEventListener("click", () => displayPokemonDetails(pokemon));
-    pokemonListElement.appendChild(listItem);
-  });
-}
-
-async function displayPokemonDetails(pokemonName) {
-  const pokemonDetailsElement = document.getElementById("details");
-  const pokemon = await fetchPokemon(pokemonName);
-
-  pokemonDetailsElement.innerHTML = `
-          <h2>Szczegóły</h2>
-          <img src="${pokemon.image}" alt="${pokemon.name}">
-          <h3>${pokemon.name}</h3>
-          <p>Id: ${pokemon.id}</p>
-          <p>Types: ${pokemon.types.join(", ")}</p>
-          <p>Base stats: ${pokemon.base_stats.join(", ")}</p>
-          <p>Height: ${pokemon.height}</p>
-          <p>Weight: ${pokemon.weight}</p>        
-      `;
-}
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   displayPokemonList();
-
-//   document
-//     .getElementById("search")
-//     .addEventListener("keypress", async function (event) {
-//       if (event.key === "Enter") {
-//         const pokemon = document.getElementById("search").value.toLowerCase();
-//         if (pokemon) {
-//           try {
-//             const response = await fetch(
-//               `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
-//             );
-//             if (!response.ok) {
-//               document.getElementById(
-//                 "error"
-//               ).innerHTML = `<p id="showError">Wystąpił błąd!</p>`;
-//               document.getElementById(
-//                 "details"
-//               ).innerHTML = `<h2>Szczegóły</h2>`;
-//               throw new Error("Błąd");
-//             }
-//             document.getElementById("error").innerHTML = ``;
-//             const data = await response.json();
-//             displayPokemonDetails(data.name);
-//           } catch (error) {
-//             console.error("Błąd");
-//           }
-//         } else {
-//           document.getElementById("error").innerHTML = ``;
-//           document.getElementById("details").innerHTML = `<h2>Szczegóły</h2>`;
-//         }
-//       }
-//     });
-// });
-
-const pokemonList = displayPokemonList();
 
 const List = ({ pokemonList }) => {
   return (
