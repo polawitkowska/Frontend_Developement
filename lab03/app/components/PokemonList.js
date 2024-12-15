d61030bffac1270051b9f2ba372b9caba19b665c
Polcia <witkowska.pola@gmail.com>
2024-12-15 17:49:03 +0100

    start

---
+++ lab03/app/components/PokemonList.js
@@ -0,0 +1,11 @@
+import styles from "../page.module.css";
+
+export default function List() {
+  return (
+    <div className={styles.page}>
+      <div>Lista Pokemonów</div>
+
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
